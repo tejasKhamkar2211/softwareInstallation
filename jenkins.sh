2e6a5c396c7743dbe77a587856b327fb16224c91
tejasKhamkar2211 <tejaskhamkar23@gmail.com>
2025-02-28 16:59:36 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,53 @@
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum upgrade
+
+sudo dnf install java-17-amazon-corretto -y
+
+sudo yum install jenkins -y
+
+sudo systemctl enable jenkins
+
+
+
+
+
+
+# resize the size of temperary folder
+sudo mount -o remount,size=2G /tmp
+
+
+#!/bin/bash
+
+# Define the file path
+FILE="/etc/fstab"
+
+# Check if the file exists, create if not
+if [ ! -f "$FILE" ]; then
+    touch "$FILE"
+fi
+
+# Check if the line already exists to avoid duplicates
+if ! grep -q "^tmpfs /tmp tmpfs defaults,size=2G 0 0" "$FILE"; then
+    # Append the line
+    echo "tmpfs /tmp tmpfs defaults,size=2G 0 0" >> "$FILE"
+    echo "Entry added to $FILE"
+else
+    echo "Entry already exists in $FILE"
+fi
+
+
+
+sudo systemctl daemon-reload
+
+
+sudo systemctl enable jenkins
+
+sudo systemctl restart jenkins
+df -h
+
+sudo systemctl status jenkins
